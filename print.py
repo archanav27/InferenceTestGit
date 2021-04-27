2b54d7de89bc2cf118be1c7ce6584828f0f511bd
archanav27 <83004979+archanav27@users.noreply.github.com>
2021-04-27 16:42:23 +0530

    Update print.py

--- print.py
+++ print.py
@@ -13,6 +13,6 @@ print('MODEL_PATH',os.getenv('MODEL_PATH'))
 print('OUTPUT_PATH',os.getenv('OUTPUT_PATH'))
 print('CODE_PATH',os.getenv('CODE_PATH'))
 
-f= open(os.getenv('MODEL_PATH') + "/DRS_Model.pl","w+")
-f.write("sampleline")
+f= open(os.getenv('OUTPUT_PATH') + "/output.csv","w+")
+f.write("1,2,3,4")
 f.close()
